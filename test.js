c8bbad9d30b8dfc8e0d5892920f0ca7461022b64
Vladimir Starkov <iamstarkov@gmail.com>
2015-06-17 06:37:41 +0200

    write real world test

--- test.js
+++ test.js
@@ -1,30 +1,34 @@
-var postcss = require('postcss');
-var expect  = require('chai').expect;
+import { equal } from 'assert';
+import postcss from 'postcss';
+import mix from './index';
 
-var plugin = require('../');
-
-var test = function (input, output, opts, done) {
-    postcss([ plugin(opts) ]).process(input).then(function (result) {
-        expect(result.css).to.eql(output);
-        expect(result.warnings()).to.be.empty;
-        done();
-    }).catch(function (error) {
-        done(error);
-    });
+const verify = function(input, output, done) {
+  postcss([ mix() ])
+    .process(input)
+    .then((result) => {
+      equal(output, result.css);
+      equal(result.warnings(), null);
+      done();
+    }).catch((error) => { done(error); });
 };
 
-describe('postcss-color-mix', function () {
+it('mix two hex colors without weight', (done)=> {
+  verify(
+    `a { color: mix(#f00, #00f); }`,
+    `a { color: #7f007f; }`,
+  done);
+});
 
-    /* Write tests here
+it('mix two hex colors with weight', (done)=> {
+  verify(
+    `a { color: mix(#f00, #00f, 25%); }`,
+    `a { color: #3f00bf; }`,
+  done);
+});
 
-    it('does something', function (done) {
-        test('a{ }', 'a{ }', { }, done);
-    });*/
-
-  /*
-  mix(#f00, #00f)  #7f007f
-  mix(#f00, #00f, 25%) => #3f00bf
-  mix(rgba(255, 0, 0, 0.5), #00f) => rgba(63, 0, 191, 0.75)
-  */
-
+it('mix hex color with rgba one with weight', (done)=> {
+  verify(
+    `a { color: mix(rgba(255, 0, 0, 0.5), #00f); }`,
+    `a { color: rgba(63, 0, 191, 0.75); }`,
+  done);
 });
