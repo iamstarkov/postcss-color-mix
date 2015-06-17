48d7486afbaf688d0eb88c97a69793aae10f259f
Vladimir Starkov <iamstarkov@gmail.com>
2015-06-18 00:34:02 +0200

    rewrite index one more time

--- index.js
+++ index.js
@@ -3,36 +3,29 @@ import balanced from 'balanced-match';
 import Color from 'color';
 import { try as postcssTry } from 'postcss-message-helpers';
 
+const mix = (c1, c2, weight) => Color(c1).mix(Color(c1), weight).hexString();
+
 const transformColor = (string, source) => {
   if (string.indexOf('mix(') === -1) {
     return string;
   }
 
-  var balancedMatches = balanced('(', ')', string);
-  if (!balancedMatches) {
-    throw new Error(`Missing closing parentheses in "${string}"`, source);
-  }
+  var mixArgs = balanced('(', ')', string);
+  if (!mixArgs) { throw new Error(`Missing closing parentheses in "${string}"`, source); }
 
-  const args = balancedMatches.body.trim().split(',').map(i => i.trim())
-  const color1 = args[0];
-  const color2 = args[1];
-  const weight = args[2];
+  const args = mixArgs.body.trim().split(',').map(i => i.trim())
 
-  const mixedColor = Color(color1).mix(Color(color2), weight).rgbaString();
-  return mixedColor;
-}
-
-const transformColorValue = (value, source) => transformColor(value, source);
+  return mix.apply(null, args);
+};
 
 const transformDecl = (decl) => {
-  if (!decl.value || decl.value.indexOf("mix(") === -1) {
+  if (!decl.value || decl.value.indexOf('mix(') === -1) {
     return;
   }
 
-  decl.value = postcssTry(transformColorValue(decl.value, decl.source));
-  return decl;
+  decl.value = postcssTry(() => transformColor(decl.value, decl.source), decl.source )
 };
 
-const mix = (css) => { css.eachDecl(transformDecl); };
-
-export default postcss.plugin('postcss-color-mix', () => mix );
+export default postcss.plugin('postcss-color-mix', () =>
+  (style) => { style.eachDecl(transformDecl); }
+);
